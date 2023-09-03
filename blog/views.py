1ddc99c7102207297b1bfbd20f77bd0f1d99bee6
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-09-03 20:48:48 +0200

    Create view to render index.html template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class ListView(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    paginate_by = 6
+    template_name = 'index.html'
